c00076a06a794a5304801c187bd7165075b555a7
oğulcan tayfur <ogulcantayfur@hotmail.com>
2020-12-22 16:38:38 +0300

    Proje dosyası ekle.

---
+++ Pi_ETicaret_Sitesi/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Pi_ETicaret_Sitesi.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
