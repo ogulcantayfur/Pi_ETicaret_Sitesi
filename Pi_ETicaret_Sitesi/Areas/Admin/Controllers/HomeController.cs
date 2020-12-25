5d392c27c031f60cc0ac539459a62a3db8accfbb
oğulcan tayfur <ogulcantayfur@hotmail.com>
2020-12-25 21:50:45 +0300

    admin paneli yapıldı, ürün ekleme sayfası oluşturuldu

--- Pi_ETicaret_Sitesi/Areas/Admin/Controllers/HomeController.cs
+++ Pi_ETicaret_Sitesi/Areas/Admin/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Pi_ETicaret_Sitesi.Interfaces;
+using Pi_ETicaret_Sitesi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,9 +13,22 @@ namespace Pi_ETicaret_Sitesi.Areas.Admin.Controllers
     [Authorize]
     public class HomeController : Controller
     {
+        private readonly IUrunRepository _urunRepository;
+
+        public HomeController(IUrunRepository urunRepository)
+        {
+            _urunRepository = urunRepository;
+        }
         public IActionResult Index()
         {
-            return View();
+            return View(_urunRepository.GetirHepsi());
         }
+
+
+        public IActionResult Ekle()
+        {
+            return View(new UrunEklemeModel());
+        }
+
     }
 }
