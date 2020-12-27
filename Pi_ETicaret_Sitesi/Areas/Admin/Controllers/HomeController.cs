4873616d7cdeacb8c5fc9912e549c6abc7d2aab5
oğulcan tayfur <ogulcantayfur@hotmail.com>
2020-12-27 14:35:45 +0300

    Urun ekleme işlemi gerçekleştirildi,ürün güncelleme ve silme de eklendi.Aynı zamanda resimler gerekli yerlerde görüntülenmeye başladı.

--- Pi_ETicaret_Sitesi/Areas/Admin/Controllers/HomeController.cs
+++ Pi_ETicaret_Sitesi/Areas/Admin/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Pi_ETicaret_Sitesi.Entities;
 using Pi_ETicaret_Sitesi.Interfaces;
 using Pi_ETicaret_Sitesi.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -30,5 +32,99 @@ namespace Pi_ETicaret_Sitesi.Areas.Admin.Controllers
             return View(new UrunEklemeModel());
         }
 
+        [HttpPost]
+        public IActionResult Ekle(UrunEklemeModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                Urun urun = new Urun();
+                if (model.resim!=null)
+                {
+                    var uzanti = Path.GetExtension(model.resim.FileName);
+                    var yeniResim = Guid.NewGuid() + uzanti;
+                    var dosyaKonumu = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/" + yeniResim);
+                    if(model.resim.ContentType=="image/jpeg"|| model.resim.ContentType == "image/png")
+                    {
+                        var stream = new FileStream(dosyaKonumu, FileMode.Create);
+                        model.resim.CopyTo(stream);
+                    }
+                    urun.resim = yeniResim;
+                }
+                
+                urun.ad = model.ad;
+                urun.fiyat = model.fiyat;
+
+                _urunRepository.Ekle(urun);
+
+                return RedirectToAction("Index", "Home");
+            }
+            return View(model);
+
+        }
+
+        public IActionResult Guncelle(int id)
+        {
+            var urun = _urunRepository.GetirIdile(id);
+
+            UrunGuncelleme model = new UrunGuncelleme
+            {
+                ad = urun.ad,
+                fiyat = urun.fiyat,
+                id = urun.id
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Guncelle (UrunGuncelleme model)
+        {
+            if (ModelState.IsValid)
+            {
+                var urun = _urunRepository.GetirIdile(model.id);
+                if (model.resim != null)
+                {
+                    var uzanti = Path.GetExtension(model.resim.FileName);
+                    var yeniResim = Guid.NewGuid() + uzanti;
+                    var dosyaKonumu = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/" + yeniResim);
+                    if (model.resim.ContentType == "image/jpeg" || model.resim.ContentType == "image/png")
+                    {
+                        var stream = new FileStream(dosyaKonumu, FileMode.Create);
+                        model.resim.CopyTo(stream);
+                    }
+                    urun.resim = yeniResim;
+                }
+
+                urun.ad = model.ad;
+                urun.fiyat = model.fiyat;
+
+                _urunRepository.Guncelle(urun);
+
+                return RedirectToAction("Index", "Home");
+            }
+            return View(model);
+        }
+
+
+        public IActionResult Sil(int id)
+        {
+            _urunRepository.Sil(new Urun { id = id });
+            return RedirectToAction("Index");
+
+        }
+
+
+
+
     }
 }
+
+
+
+
+
+
+
+
+
+       
---
+++ Pi_ETicaret_Sitesi/Models/UrunGuncelleme.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pi_ETicaret_Sitesi.Models
+{
+    public class UrunGuncelleme
+    {
+        public int id { get; set; }
+        [Required(ErrorMessage = "Ad alanı gereklidir")]
+        public string ad { get; set; }
+
+        [Range(0, double.MaxValue, ErrorMessage = "Geçerli bir fiyat giriniz.")]
+        public decimal fiyat { get; set; }
+        public IFormFile resim { get; set; }
+    }
+}
